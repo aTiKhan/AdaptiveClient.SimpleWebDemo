80667ebde74ca3af240c34e790d884b70ccd6978
Sam <sam.wheat@outlook.com>
2018-06-23 15:48:00 -0700

    Update AdaptiveClient

--- AdaptiveClient.WebDemo/AdaptiveClient.WebDemo/AutofacHelper.cs
+++ AdaptiveClient.WebDemo/AdaptiveClient.WebDemo/AutofacHelper.cs
@@ -16,14 +16,15 @@ namespace AdaptiveClient.WebDemo
 
     public static class DataProvider
     {
-        public static string MSSQL = "MSSQL";
-        public static string MySQL = "MySQL";
-        //public static string Oracle = "Oracle";
+        public const string MSSQL = "MSSQL";
+        public const string MySQL = "MySQL";
+        public const string HTTP = "HTTP";
+        //public const string Oracle = "Oracle";
     }
 
-    public enum API_Name
+    public static class API_Name
     {
-        UsersAPI
+        public const string UsersAPI = "UsersAPI";
         // ,YourAPIName
     }
 
@@ -44,13 +45,13 @@ namespace AdaptiveClient.WebDemo
     {
         public static void RegisterComponents(ContainerBuilder builder)
         {
-            AutofacRegistrationHelper registrationHelper = new AutofacRegistrationHelper(builder);
+            RegistrationHelper registrationHelper = new RegistrationHelper(builder);
             IEnumerable<IEndPointConfiguration> endPoints = ReadEndPoints();
 
             // API Name is an arbitrary name of your choosing that AdaptiveClient uses to link interfaces (IUsersService) to the
             // EndPoints that expose them (Prod_SQL_01, Prod_WebAPI_01).  The API_Name used here must match the API_Name 
             // of related EndPoints in EndPoints.json file.
-            string apiName = API_Name.UsersAPI.ToString();
+            
 
             // register endPoints before registering clients
             registrationHelper.RegisterEndPoints(endPoints);
@@ -58,13 +59,13 @@ namespace AdaptiveClient.WebDemo
             // register clients
 
             // We can register a url as a connection string.  If we pass AdaptiveClient an EndPoint of type HTTP it will return an instance of UsersWebAPIClient
-            registrationHelper.Register<UsersWebAPIClient, IUsersService>(EndPointType.HTTP, apiName);
+            registrationHelper.RegisterService<UsersWebAPIClient, IUsersService>(EndPointType.HTTP, API_Name.UsersAPI, DataProvider.HTTP);
 
             // If our EndPoint has a DataProvider type of MSSQL, AdaptiveClient will return an instance of UsersService_MSSQL which (fictitiously) contains SQL for Microsoft SQL Server.
-            registrationHelper.Register<UsersService_MSSQL, IUsersService>(EndPointType.InProcess, apiName, DataProvider.MSSQL);
+            registrationHelper.RegisterService<UsersService_MSSQL, IUsersService>(EndPointType.InProcess, API_Name.UsersAPI, DataProvider.MSSQL);
 
             // If our EndPoint has a DataProvider type of MySQL, AdaptiveClient will return an instance of UsersService_MySQL which (fictitiously) contains SQL for MySQL.
-            registrationHelper.Register<UsersService_MySQL, IUsersService>(EndPointType.InProcess, apiName, DataProvider.MySQL);
+            registrationHelper.RegisterService<UsersService_MySQL, IUsersService>(EndPointType.InProcess, API_Name.UsersAPI, DataProvider.MySQL);
 
             // register logger (optional)
             registrationHelper.RegisterLogger(logMessage => Logger.Message = logMessage);
@@ -77,11 +78,10 @@ namespace AdaptiveClient.WebDemo
         public static void RegisterFallbackMocks(ContainerBuilder builder)
         {
             // this method is for mocks - see RegisterComponents for examples of how to register your components with AdaptiveClient.
-            AutofacRegistrationHelper registrationHelper = new AutofacRegistrationHelper(builder);
+            RegistrationHelper registrationHelper = new RegistrationHelper(builder);
             IEnumerable<IEndPointConfiguration> endPoints = ReadEndPoints();
-            string apiName = API_Name.UsersAPI.ToString();
             registrationHelper.RegisterEndPoints(endPoints);
-            registrationHelper.Register<UsersWebAPIClient, IUsersService>(EndPointType.HTTP, apiName);
+            registrationHelper.RegisterService<UsersWebAPIClient, IUsersService>(EndPointType.HTTP, API_Name.UsersAPI, DataProvider.HTTP);
             var usersServiceMock = new Mock<IUsersService>();
             usersServiceMock.Setup(x => x.SaveUser(It.IsAny<User>())).Throws(new Exception("Cant find database server."));
             usersServiceMock.Setup(x => x.GetUserByID(It.IsAny<int>())).Throws(new Exception("Cant find database server."));
@@ -92,13 +92,12 @@ namespace AdaptiveClient.WebDemo
 
         public static void RegisterMySQLMocks(ContainerBuilder builder)
         {
-            AutofacRegistrationHelper registrationHelper = new AutofacRegistrationHelper(builder);
+            RegistrationHelper registrationHelper = new RegistrationHelper(builder);
             IEnumerable<IEndPointConfiguration> endPoints = ReadEndPoints().Where(x => x.ProviderName != DataProvider.MSSQL);
-            string apiName = API_Name.UsersAPI.ToString();
             registrationHelper.RegisterEndPoints(endPoints);
-            registrationHelper.Register<UsersWebAPIClient, IUsersService>(EndPointType.HTTP, apiName);
-            registrationHelper.Register<UsersService_MSSQL, IUsersService>(EndPointType.InProcess, apiName, DataProvider.MSSQL);
-            registrationHelper.Register<UsersService_MySQL, IUsersService>(EndPointType.InProcess, apiName, DataProvider.MySQL);
+            registrationHelper.RegisterService<UsersWebAPIClient, IUsersService>(EndPointType.HTTP, API_Name.UsersAPI, DataProvider.HTTP);
+            registrationHelper.RegisterService<UsersService_MSSQL, IUsersService>(EndPointType.InProcess, API_Name.UsersAPI, DataProvider.MSSQL);
+            registrationHelper.RegisterService<UsersService_MySQL, IUsersService>(EndPointType.InProcess, API_Name.UsersAPI, DataProvider.MySQL);
             registrationHelper.RegisterLogger(logMessage => Logger.Message = logMessage);
             builder.RegisterType<Demo>();
         }
